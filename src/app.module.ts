2f621c03eb707ff9804d9c9d18ada94d2e48f5a1
Abel Separovich <separovi@gmail.com>
2022-04-30 15:42:49 -0300

    setting up swagger and yarn

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CategoryModule } from './category/category.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [CategoryModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    CategoryModule,
+    ProductModule,
+  ],
 })
 export class AppModule {}
