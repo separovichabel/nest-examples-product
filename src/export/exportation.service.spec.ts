a74423cafc3d898ee55519fe3068e11618fac2cb
Abel Separovich <separovi@gmail.com>
2022-05-04 22:50:34 -0300

    implementing some tests to exportation

--- src/export/exportation.service.spec.ts
+++ src/export/exportation.service.spec.ts
@@ -1,15 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { Product } from 'src/product/entities/product.entity';
+import { Writable } from 'stream';
 
-const fsMock = {
+const fsPMock = {
   readdir: jest.fn(),
   mkdir: jest.fn(),
-}
+};
 
-jest.mock('fs/promises', ()=> fsMock)
+
+const exportService = {
+  findAll: jest.fn(),
+  create: jest.fn(),
+};
+
+jest.mock('fs/promises', () => fsPMock);
 
 import { ExportationService } from './exportation.service';
-
 
 describe('ExportationService', () => {
   let service: ExportationService<Product>;
@@ -21,8 +27,12 @@ describe('ExportationService', () => {
 
     service = module.get<ExportationService<Product>>(ExportationService);
 
-    Object.keys(fsMock).forEach((key) => {
-      fsMock[key].mockReset();
+    Object.keys(fsPMock).forEach((key) => {
+      fsPMock[key].mockReset();
+    });
+
+    Object.keys(exportService).forEach((key) => {
+      exportService[key].mockReset();
     });
   });
 
@@ -32,17 +42,33 @@ describe('ExportationService', () => {
 
   describe('onModuleInit()', () => {
     it('should not create temp if it exists', () => {
-      fsMock.readdir.mockResolvedValue(['temp', 'pasta2'])
-      service.onModuleInit()
-      expect(fsMock.readdir.mock.calls.length).toBe(1);
-      expect(fsMock.mkdir.mock.calls.length).toBe(0);
+      fsPMock.readdir.mockResolvedValue(['temp', 'pasta2']);
+      service.onModuleInit();
+      expect(fsPMock.readdir.mock.calls.length).toBe(1);
+      expect(fsPMock.mkdir.mock.calls.length).toBe(0);
     });
-  
+
     it('should create temp if it does not exists', async () => {
-      fsMock.readdir.mockResolvedValue(['pasta1', 'pasta2'])
-      await service.onModuleInit()
-      expect(fsMock.readdir.mock.calls.length).toBe(1);
-      expect(fsMock.mkdir.mock.calls.length).toBe(1);
+      fsPMock.readdir.mockResolvedValue(['pasta1', 'pasta2']);
+      await service.onModuleInit();
+      expect(fsPMock.readdir.mock.calls.length).toBe(1);
+      expect(fsPMock.mkdir.mock.calls.length).toBe(1);
     });
-  })
+  });
+
+  describe('exportData()', () => {
+    it('should work', async () => {
+      const fn = { write: jest.fn(), end: jest.fn() };
+
+      exportService.findAll
+        .mockImplementationOnce(() => [{ a: 1 }, { a: 3 }, { a: 2 }])
+        .mockImplementationOnce(() => []);
+      const writable = new Writable(fn);
+      await service.exportData({}, writable, exportService);
+
+      expect(exportService.findAll.mock.calls.length).toBe(2);
+      expect(fn.write.mock.calls.length).toBe(1);
+      // expect(fn.end.mock.calls.length).toBe(1)
+    });
+  });
 });
